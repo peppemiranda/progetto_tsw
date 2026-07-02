b52e157858c712a834f0d4a4c5000ca026104e2a
pepemiranda <peppiniellomiranda@gmail.com>
2026-07-02 19:33:35 +0200

    Aggiunto costruttore vuoto e costruttore con parametri

--- src/main/java/model/Ordine.java
+++ src/main/java/model/Ordine.java
@@ -8,4 +8,16 @@ public class Ordine {
     private int idUtente; // Questa è la nostra Chiave Esterna (FK) verso l'utente
     private Date dataAcquisto;
     private double totaleOrdine;
+    
+    // Costruttore vuoto
+    public Ordine() {
+    }
+
+    //Costruttore con parametri
+    public Ordine(int idOrdine, int idUtente, Date dataAcquisto, double totaleOrdine) {
+        this.idOrdine = idOrdine;
+        this.idUtente = idUtente;
+        this.dataAcquisto = dataAcquisto;
+        this.totaleOrdine = totaleOrdine;
+    }
 }
