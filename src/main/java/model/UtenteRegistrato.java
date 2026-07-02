740bc12c0383679b14aa0849dd2d305db479d052
pepemiranda <peppiniellomiranda@gmail.com>
2026-07-02 19:11:55 +0200

    Aggiunti i metodi getter e setter

--- src/main/java/model/UtenteRegistrato.java
+++ src/main/java/model/UtenteRegistrato.java
@@ -26,4 +26,74 @@ public UtenteRegistrato(int idUtente, String nome, String cognome, String email,
     this.indirizzoSpedizione = indirizzoSpedizione;
  	}
 
+
+//Metodi get e set
+
+	public int getIdUtente() {
+		return idUtente;
+		}
+
+	public void setIdUtente(int idUtente) {
+		this.idUtente = idUtente;
+		}
+	
+	
+
+	public String getNome() {
+		return nome;
+		}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+		}
+
+	
+	
+	public String getCognome() {
+		return cognome;
+		}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+		}
+
+	
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+		}
+
+	
+	
+	public String getPasswordHash() {
+		return passwordHash;
+		}
+
+	public void setPasswordHash(String passwordHash) {
+		this.passwordHash = passwordHash;
+		}
+
+	
+	
+	public String getRuolo() {
+		return ruolo;
+		}
+
+	public void setRuolo(String ruolo) {
+		this.ruolo = ruolo;
+		}
+
+	
+	
+	public String getIndirizzoSpedizione() {
+		return indirizzoSpedizione;
+		}
+
+	public void setIndirizzoSpedizione(String indirizzoSpedizione) {
+		this.indirizzoSpedizione = indirizzoSpedizione;
+		}
 }
