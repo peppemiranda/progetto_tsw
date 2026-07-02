8222cbb458c783a278bed5fcc22abc1eff351263
pepemiranda <peppiniellomiranda@gmail.com>
2026-07-02 18:58:49 +0200

    Creazione del file UtenteRegistrato.java nel package model, è una Java Beans, e rappresenta la tabella Utente_Registrato del nostro database. Facciamo ciò per far comunicare Java con il database

---
+++ src/main/java/model/UtenteRegistrato.java
@@ -0,0 +1,13 @@
+package model;
+
+public class UtenteRegistrato {
+    
+    private int idUtente;
+    private String nome;
+    private String cognome;
+    private String email;
+    private String passwordHash;
+    private String ruolo;
+    private String indirizzoSpedizione;
+
+}
