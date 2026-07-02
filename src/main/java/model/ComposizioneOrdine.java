231d9cb4dd78b20b1740d1c64d099e228002f4e9
pepemiranda <peppiniellomiranda@gmail.com>
2026-07-02 19:43:36 +0200

    Aggiunto costruttore vuoto e con i parametri

--- src/main/java/model/ComposizioneOrdine.java
+++ src/main/java/model/ComposizioneOrdine.java
@@ -7,5 +7,15 @@ public class ComposizioneOrdine {
     private double prezzoAcquisto;
     private int quantitaScelta;
 
-    
+    // Costruttore vuoto
+    public ComposizioneOrdine() {
+    }
+
+    //Costruttore con parametri
+    public ComposizioneOrdine(int idOrdine, int idScarpa, double prezzoAcquisto, int quantitaScelta) {
+        this.idOrdine = idOrdine;
+        this.idScarpa = idScarpa;
+        this.prezzoAcquisto = prezzoAcquisto;
+        this.quantitaScelta = quantitaScelta;
+    }
 }
