e19c1b480255bdb4eaddc9d7acf5de0737f02546
pepemiranda <peppiniellomiranda@gmail.com>
2026-07-02 19:23:20 +0200

    Aggiunto costruttore vuoto e costruttore con i parametri

--- src/main/java/model/Scarpa.java
+++ src/main/java/model/Scarpa.java
@@ -7,4 +7,19 @@ public class Scarpa {
     private String terreno;
     private double prezzoAttuale;
     private int pezziMagazzino;
+    
+ // 2. Costruttore vuoto
+    public Scarpa() {
+    }
+
+    // 3. Costruttore con parametri
+    public Scarpa(int idScarpa, String marca, String modello, String terreno, 
+                  double prezzoAttuale, int pezziMagazzino) {
+        this.idScarpa = idScarpa;
+        this.marca = marca;
+        this.modello = modello;
+        this.terreno = terreno;
+        this.prezzoAttuale = prezzoAttuale;
+        this.pezziMagazzino = pezziMagazzino;
+    }
 }
