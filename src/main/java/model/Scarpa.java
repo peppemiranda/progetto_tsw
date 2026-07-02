7f1152d36d5836517a3455f4611d40361bd0b2bc
pepemiranda <peppiniellomiranda@gmail.com>
2026-07-02 19:21:50 +0200

    Creazione del file Scarpa.java nel package model, è una Java Beans, e rappresenta la tabella Scarpa del nostro database. Facciamo ciò per far comunicare Java con il database

---
+++ src/main/java/model/Scarpa.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Scarpa {
+	private int idScarpa;
+    private String marca;
+    private String modello;
+    private String terreno;
+    private double prezzoAttuale;
+    private int pezziMagazzino;
+}
