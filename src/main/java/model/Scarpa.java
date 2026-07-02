36f9f0c30c7c53dfa7607722bb920fcda484be21
pepemiranda <peppiniellomiranda@gmail.com>
2026-07-02 19:26:12 +0200

    Aggiunti metodi getter e setter

--- src/main/java/model/Scarpa.java
+++ src/main/java/model/Scarpa.java
@@ -22,4 +22,63 @@ public class Scarpa {
         this.prezzoAttuale = prezzoAttuale;
         this.pezziMagazzino = pezziMagazzino;
     }
+    
+ // 4. Metodi Get e Set
+    public int getIdScarpa() {
+        return idScarpa;
+    }
+
+    public void setIdScarpa(int idScarpa) {
+        this.idScarpa = idScarpa;
+    }
+
+    
+    
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    
+    
+    public String getModello() {
+        return modello;
+    }
+
+    public void setModello(String modello) {
+        this.modello = modello;
+    }
+
+    
+    
+    public String getTerreno() {
+        return terreno;
+    }
+
+    public void setTerreno(String terreno) {
+        this.terreno = terreno;
+    }
+
+    
+    
+    public double getPrezzoAttuale() {
+        return prezzoAttuale;
+    }
+
+    public void setPrezzoAttuale(double prezzoAttuale) {
+        this.prezzoAttuale = prezzoAttuale;
+    }
+
+    
+    
+    public int getPezziMagazzino() {
+        return pezziMagazzino;
+    }
+
+    public void setPezziMagazzino(int pezziMagazzino) {
+        this.pezziMagazzino = pezziMagazzino;
+    }
 }
