22ade5a3e79503ab7abddf5f1abf717f42e32505
pepemiranda <peppiniellomiranda@gmail.com>
2026-07-02 19:06:15 +0200

    Aggiunto costruttore vuoto e costruttore con parametri

--- src/main/java/model/UtenteRegistrato.java
+++ src/main/java/model/UtenteRegistrato.java
@@ -10,4 +10,20 @@ public class UtenteRegistrato {
     private String ruolo;
     private String indirizzoSpedizione;
 
+
+// Costruttore vuoto 
+public UtenteRegistrato() {
+}
+
+// Costruttore con parametri
+public UtenteRegistrato(int idUtente, String nome, String cognome, String email, String passwordHash, String ruolo, String indirizzoSpedizione) {
+    this.idUtente = idUtente;
+    this.nome = nome;
+    this.cognome = cognome;
+    this.email = email;
+    this.passwordHash = passwordHash;
+    this.ruolo = ruolo;
+    this.indirizzoSpedizione = indirizzoSpedizione;
+ 	}
+
 }
