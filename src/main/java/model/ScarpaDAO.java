3d282fc8449648d4261a8ed260fd7c052108db72
pepemiranda <peppiniellomiranda@gmail.com>
2026-07-03 18:27:17 +0200

    Interfaccia, in essa sono presenti le due operazioni che si possono fare su Scarpa. I due metodi verranno implementati nella classe "ScarpaDAODataSource"

---
+++ src/main/java/model/ScarpaDAO.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.sql.SQLException;
+import java.util.Collection;
+
+public interface ScarpaDAO {
+    
+    // Recupera una singola scarpa dato il suo ID (per la pagina dettaglio)
+    Scarpa doRetrieveByKey(int idScarpa) throws SQLException;
+    
+    // Recupera l'intero catalogo di scarpe (per la griglia prodotti)
+    Collection<Scarpa> doRetrieveAll() throws SQLException;
+}
