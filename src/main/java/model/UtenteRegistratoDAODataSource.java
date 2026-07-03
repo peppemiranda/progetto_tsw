ee457d798450cd17753fe1f3a57051231c3c77d6
pepemiranda <peppiniellomiranda@gmail.com>
2026-07-03 18:15:28 +0200

    Classe nella quale implementiamo i metodi dell'interfaccia UtenteRegistratoDAO

---
+++ src/main/java/model/UtenteRegistratoDAODataSource.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UtenteRegistratoDAODataSource implements UtenteRegistratoDAO {
+
+}
