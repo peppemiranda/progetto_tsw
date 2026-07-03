4adb7c9c09ac67f2c9d7ff389474fadb9738c703
pepemiranda <peppiniellomiranda@gmail.com>
2026-07-03 18:18:05 +0200

    Aggiunto il metodo doSave

--- src/main/java/model/UtenteRegistratoDAODataSource.java
+++ src/main/java/model/UtenteRegistratoDAODataSource.java
@@ -6,5 +6,23 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UtenteRegistratoDAODataSource implements UtenteRegistratoDAO {
-
+	
+	public void doSave(UtenteRegistrato utente) throws SQLException {
+		
+        // Query SQL parametrizzata per evitare SQL Injection
+        String query = "INSERT INTO Utente_Registrato (Nome, Cognome, Email, PasswordHash, Ruolo, Indirizzo_Spedizione) VALUES (?, ?, ?, ?, ?, ?)";
+        
+        try (Connection con = ConPool.getConnection();
+             PreparedStatement ps = con.prepareStatement(query)) {
+            
+            ps.setString(1, utente.getNome());
+            ps.setString(2, utente.getCognome());
+            ps.setString(3, utente.getEmail());
+            ps.setString(4, utente.getPasswordHash());
+            ps.setString(5, utente.getRuolo());
+            ps.setString(6, utente.getIndirizzoSpedizione());
+            
+            ps.executeUpdate();
+        }
+    }
 }
