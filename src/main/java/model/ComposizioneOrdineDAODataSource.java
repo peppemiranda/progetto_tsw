2534dbc7abfba983c1dd4ccc40f9c9ce3ff9472d
pepemiranda <peppiniellomiranda@gmail.com>
2026-07-03 19:11:42 +0200

    Implementato il metodo doSave

--- src/main/java/model/ComposizioneOrdineDAODataSource.java
+++ src/main/java/model/ComposizioneOrdineDAODataSource.java
@@ -7,6 +7,23 @@ import java.sql.SQLException;
 import java.util.Collection;
 import java.util.LinkedList;
 
-public class ComposizioneOrdineDAODataSource implements ComposizioneOrdineDAO{
-
+public class ComposizioneOrdineDAODataSource implements ComposizioneOrdineDAO {
+	
+	@Override
+    public void doSave(ComposizioneOrdine composizione) throws SQLException {
+		
+        // Inserimento nella tabella con la chiave primaria composta
+        String query = "INSERT INTO Composizione_Ordine (ID_Ordine, ID_Scarpa, Prezzo_Acquisto, Quantita_Scelta) VALUES (?, ?, ?, ?)";
+        
+        try (Connection con = ConPool.getConnection();
+             PreparedStatement ps = con.prepareStatement(query)) {
+            
+            ps.setInt(1, composizione.getIdOrdine());
+            ps.setInt(2, composizione.getIdScarpa());
+            ps.setDouble(3, composizione.getPrezzoAcquisto());
+            ps.setInt(4, composizione.getQuantitaScelta());
+            
+            ps.executeUpdate();
+        }
+    }
 }
