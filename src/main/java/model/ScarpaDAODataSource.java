1d76b1d42ad299b7419b14ac78062f03448d2746
pepemiranda <peppiniellomiranda@gmail.com>
2026-07-03 18:29:33 +0200

    Classe nella quale implementiamo i metodi dell'interfaccia ScarpaDAO

---
+++ src/main/java/model/ScarpaDAODataSource.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.LinkedList;
+
+public class ScarpaDAODataSource implements ScarpaDAO {
+
+}
