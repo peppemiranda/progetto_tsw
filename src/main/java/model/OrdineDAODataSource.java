6c728e5b3d4063ecfc94405fbc0bee42da434e8e
pepemiranda <peppiniellomiranda@gmail.com>
2026-07-03 18:48:02 +0200

    Classe nella quale implementiamo i metodi dell'interfaccia OrdineDAO

---
+++ src/main/java/model/OrdineDAODataSource.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement; 		//Per recuperare l'ID generato
+import java.util.Collection;
+import java.util.LinkedList;
+
+public class OrdineDAODataSource implements OrdineDAO {
+
+}
