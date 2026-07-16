f942faccc5888fdd20814bf179b1a7754e360c50
pepemiranda <peppiniellomiranda@gmail.com>
2026-07-16 22:14:15 +0200

    Il codice prima di doFilter

--- src/main/java/control/AdminFilter.java
+++ src/main/java/control/AdminFilter.java
@@ -1,5 +1,37 @@
 package control;
 
-public class AdminFilter {
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
 
+//Il Filter controlla "AdminServlet"
+@WebFilter(urlPatterns = {"/AdminServlet"})
+public class AdminFilter extends HttpFilter {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+		throws IOException, ServletException {
+		
+		boolean isLoggato;
+		
+		//Recuperiamo la sessione senza crearne una nuova se non esiste
+        HttpSession session = request.getSession(false);
+        
+        //Verifichiamo se c'è una sessione e se c'è un utente loggato dentro
+        isLoggato = (session != null && session.getAttribute("utenteLoggato") != null);
+        
+        if (isLoggato) {
+        	
+        	//Estraiamo l'oggetto utente
+            model.UtenteRegistrato utente = (model.UtenteRegistrato) session.getAttribute("utenteLoggato");
+            
+            //Controlliamo il RUOLO
+            if ("Admin".equalsIgnoreCase(utente.getRuolo())) {
+            	
+                //Quindi se l'utente è l'Admin, allora passa
+                chain.doFilter(request, response);
+                
+            }
+	}
 }
