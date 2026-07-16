dc83057763499d0872ea25a5e4548a204da61645
pepemiranda <peppiniellomiranda@gmail.com>
2026-07-16 21:57:51 +0200

    Aggiunti nuovi metodi all'interfaccia, che adesso implementaremo in ScarpaDAODataSource. I metodi servono per l'admin

--- src/main/java/model/ScarpaDAO.java
+++ src/main/java/model/ScarpaDAO.java
@@ -10,4 +10,11 @@ public interface ScarpaDAO {
     
     // Recupera l'intero catalogo di scarpe (per la griglia prodotti)
     Collection<Scarpa> doRetrieveAll() throws SQLException;
+    
+    //Metodi per l'admin
+    void doSave(Scarpa scarpa) throws SQLException;
+    
+    void doUpdate(Scarpa scarpa) throws SQLException;
+    
+    void doDelete(int idScarpa) throws SQLException;
 }
