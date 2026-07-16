090ec81f2e643b7d90e405a5e9ae96985315da18
pepemiranda <peppiniellomiranda@gmail.com>
2026-07-16 17:24:00 +0200

    Modificato il metodo doPost

--- src/main/java/control/CarrelloServlet.java
+++ src/main/java/control/CarrelloServlet.java
@@ -46,9 +46,54 @@ public class CarrelloServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
+	
+	//Metodo doPost che lo usiamo per modificare i dati del carrello(aggiungere, rimuovere, svuotare)
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+jakarta.servlet.http.HttpSession session = request.getSession();
+        
+        java.util.ArrayList<model.Scarpa> carrello = (java.util.ArrayList<model.Scarpa>) session.getAttribute("carrello");
+        if (carrello == null) {
+            carrello = new java.util.ArrayList<>();
+            session.setAttribute("carrello", carrello);
+        }
+
+        String azione = request.getParameter("azione");
+
+        if (azione != null) {
+            try {
+                if (azione.equals("aggiungi")) {
+                    int idScarpa = Integer.parseInt(request.getParameter("id"));
+                    model.ScarpaDAODataSource dao = new model.ScarpaDAODataSource();
+                    model.Scarpa scarpaDaAggiungere = dao.doRetrieveByKey(idScarpa);
+                    
+                    if (scarpaDaAggiungere != null) {
+                        carrello.add(scarpaDaAggiungere);
+                    }
+                } 
+                else if (azione.equals("rimuovi")) {
+                    int idScarpa = Integer.parseInt(request.getParameter("id"));
+                    for (int i = 0; i < carrello.size(); i++) {
+                        if (carrello.get(i).getIdScarpa() == idScarpa) {
+                            carrello.remove(i);
+                            break;
+                        }
+                    }
+                }
+                else if (azione.equals("svuota")) {
+                    carrello.clear();
+                }
+            } catch (NumberFormatException | java.sql.SQLException e) {
+                e.printStackTrace();
+                // In caso di errore SQL mandiamo alla pagina di errore per sicurezza
+                request.getRequestDispatcher("/WEB-INF/views/common/errore.jsp").forward(request, response);
+                return;
+            }
+        }
+        
+        // Dopo aver modificato i dati(nel metodo doPost), facciamo un redirect verso la stessa Servlet per evitare che
+        // ricaricando la pagina l'utente compri due volte lo stesso prodotto
+        response.sendRedirect("CarrelloServlet");
 	}
 
 }
