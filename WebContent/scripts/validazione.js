993957ca167ac462fb96605019468e892c89b665
pepemiranda <peppiniellomiranda@gmail.com>
2026-07-19 02:07:57 +0200

    Continuazione del file "validazione.js"

--- WebContent/scripts/validazione.js
+++ WebContent/scripts/validazione.js
@@ -1,3 +1,16 @@
-/**
- * 
- */
+
+const patternNome = /^[A-Za-z]+$/; 
+
+function validateFormElem(formElem, pattern, span, errorMessage) {
+    if (formElem.value.match(pattern)) {
+        formElem.classList.remove("error");
+        span.style.color = "black";
+        span.innerHTML = "";
+        return true;
+    } else {
+        formElem.classList.add("error");
+        span.innerHTML = errorMessage;
+        span.style.color = "red";
+        return false;
+    }
+}
